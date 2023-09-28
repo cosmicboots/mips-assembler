4ba1da0f8be3b22ca163dcd9695524e846ef82c7
Thom Dickson <td3of4@gmail.com>
2023-09-28 18:37:25 -0400

    Cleanup testing

--- tests.sh
+++ tests.sh
@@ -2,10 +2,13 @@
 
 tests=($(find examples/MA/ -type f -name '*.hex'))
 
+make
+
 if [[ $1 ]]
 then
     src="$(dirname $1)/$(basename "$1" ".hex").asm"
-    difft <(./build/main "$src" | awk '{print $1}') <(awk '{print $1}' "$1")
+    #difft <(./build/main "$src" | awk '{print $1}') <(awk '{print $1}' "$1")
+    diff <(./build/main "$src" | awk '{print $1}') <(awk '{print $1}' "$1")
     ./build/main "$src"
 else
     total=0
@@ -14,12 +17,10 @@ else
     do
         src="$(dirname $test)/$(basename "$test" ".hex").asm"
         echo " |=== Checking: [$src] ===|"
-        difft --exit-code <(./build/main "$src" 2>/dev/null | awk '{print $1}') <(awk '{print $1}' "$test")
-        if [[ $? -ne 0 ]]
+        #difft --exit-code <(./build/main "$src" 2>/dev/null | awk '{print $1}') <(awk '{print $1}' "$test")
+        diff <(./build/main "$src" 2>/dev/null | awk '{print $1}') <(awk '{print $1}' "$test")
+        if [[ $? -eq 0 ]]
         then
-            #exit 1
-            echo
-        else
             good=$(($good + 1))
         fi
         total=$(($total + 1))
